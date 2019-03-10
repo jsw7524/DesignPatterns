2011afc0bf1ebc00e3a49c91a660a0eb9e77e8a4
jsw7524 <jsw7524@gmail.com>
2019-03-10 16:00:14 +0800

    DelegateAdapter

---
+++ DelegateAdapter/PrintBanner.cs
@@ -0,0 +1,21 @@
+﻿namespace DelegateAdapter
+{
+    class PrintBanner : Print
+    {
+        private Banner banner;
+
+        public PrintBanner(string s)
+        {
+            banner = new Banner(s);
+        }
+        public override void PrintWeak()
+        {
+            banner.ShowWithParen();
+        }
+
+        public override void PrintStrong()
+        {
+            banner.ShowWithAster();
+        }
+    }
+}
---
+++ DelegateAdapter/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateAdapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DelegateAdapter();
+            Console.ReadLine();
+        }
+
+        private static void DelegateAdapter()
+        {
+            Print p = new PrintBanner("Hello DelegateAdapter");
+            p.PrintWeak();
+            p.PrintStrong();
+        }
+    }
+}
